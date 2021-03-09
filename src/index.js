d966f53a36e5f5a3959232be45e059a2a19ec95e
spiderVS <fuzzy003@gmail.com>
2021-03-09 15:34:22 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  	
+let strToArray = str.split(''); // преобразуем строку в массив символов
+//console.log(strToArray);
+  
+// поднимаем эл-ты массива bracketsConfig с нижнего уровня, делаем одноуровневый массив		
+let bCflat = [];
+for (let i = 0; i < bracketsConfig.length; i++) {
+bCflat = bCflat.concat(bracketsConfig[i]);
+}
+
+//console.log(bCflat);
+
+let flag_splice = 0;
+let flag = 0;
+while (flag <= flag_splice) {
+	flag++;
+	for (let k = 0; k < bCflat.length; k=k+2) {		
+		for (let l = 0; l < strToArray.length; l++) {				
+     		if (strToArray[l] == bCflat[k] && strToArray[l+1] == bCflat[k+1]) {
+     			strToArray.splice(l, 2);
+				//console.log (strToArray);
+				l = l-1;
+				flag_splice++;
+     		}
+    	}			
+ 	}
+}		
+	//console.log (strToArray);
+	if (strToArray != 0) return false;
+		else return true;
 }
